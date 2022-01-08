623846ee5e47cd24d7dd23cfff08f6cdaed6e5c0
Harvey Wu <harveywu@mail.ru>
2022-01-08 22:36:25 +0800

    fix: optimizing project frame and style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,38 @@
 // main.js
+
+function tips() {
+    console.log("*****************************");
+    console.log("Orata - The Theme of Jekyll.");
+    console.log("Designed by Harvey Wu.");
+    console.log("*****************************");
+}
+
+// Full screen menu.
+function fullMenuController() {
+    const menu = document.querySelector(".nav-menu");
+    const menuBox = document.querySelector(".nav-menu-box");
+    const fullMenu = document.querySelector(".fullmenu");
+    menu.addEventListener("click", () => {
+        menuBox.classList.toggle("open");
+        fullMenu.classList.toggle("open");
+    });
+}
+
+// Works slider animation.
+function worksSliderAnimationController() {
+    const worksList = document.querySelector(".works-items-list");
+    const worksItem = document.querySelector(".works-items-item");
+
+    worksList.appendChild(worksItem);
+    worksList.animate(
+        [{
+            left: "-" + worksItem.clientWidth+"px"
+        }], {
+            easing: ['ease']
+        }, 3000,
+        function () {
+            worksList.appendChild(worksItem.firstChild);
+            worksList.style.left = "";
+        }
+    );
+}
