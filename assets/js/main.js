08f77de5bf957be04e48591b0eaf5db6677f793c
Harvey Wu <harveywu@mail.ru>
2022-01-16 21:58:56 +0800

    feat: Build base 'Leccia' framework.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,15 @@
-/* main.js
-*
-*  Copyright 2021 Harvey Wu.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*/
+// main.js
 
+// Initial.
 function initial() {
     console.log("Leccia - The Theme of Jekyll.\nDesigned by Harvey Wu.");
 }
 
 // Full-screen menu for mobile.
-function fullMenuController() {
+function menuController() {
     const menu = document.querySelector(".nav-menu");
     const menuBox = document.querySelector(".nav-menu-box");
-    const fullMenu = document.querySelector(".fullmenu");
+    const fullMenu = document.querySelector(".menu");
     menu.addEventListener("click", () => {
         menuBox.classList.toggle("open");
         fullMenu.classList.toggle("open");
